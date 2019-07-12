2d5e46becfc64ce434647558cea09a0b62a315e8
Luis R. Palanca <luis@typeform.com>
2019-07-12 16:28:02 +0200

    Add example challenge

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
+import * as cocoSsd from "@tensorflow-models/coco-ssd"
+import "@tensorflow/tfjs"
+import { isMobile } from 'react-device-detect'
+import "./styles.css"
 
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import "@tensorflow/tfjs";
-import "./styles.css";
+const questions = [
+  {
+    message: 'Welcome to the challenge. The first goal is to find a dog (and also cuddle him)',
+    label: 'dog'
+  },
+  {
+    message: 'Done, nice dog. Once you have the dog, you should look for a Windows 98 book, pure wisdom!',
+    label: 'book'
+  },
+  {
+    message: 'Challenge finished! You will get some chocolate',
+    label: 'end'
+  }
+]
 
 class App extends React.Component {
   videoRef = React.createRef();
   canvasRef = React.createRef();
 
+  constructor() {
+    super()
+    this.state = { question: 0 };
+  }
+
   componentDidMount() {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const facingMode = isMobile ? { exact: 'environment' } : 'user'
       const webCamPromise = navigator.mediaDevices
         .getUserMedia({
           audio: false,
           video: {
-            facingMode: { exact: "environment" }
+            facingMode: facingMode
           }
         })
         .then(stream => {
@@ -55,6 +76,9 @@ class App extends React.Component {
     ctx.font = font;
     ctx.textBaseline = "top";
     predictions.forEach(prediction => {
+      if (questions[this.state.question].label === prediction.class) {
+        this.setState({question: this.state.question + 1})
+      }
       const x = prediction.bbox[0];
       const y = prediction.bbox[1];
       const width = prediction.bbox[2];
@@ -70,35 +94,46 @@ class App extends React.Component {
       ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
     });
 
-    predictions.forEach(prediction => {
+    /*predictions.forEach(prediction => {
       const x = prediction.bbox[0];
       const y = prediction.bbox[1];
       // Draw the text last to ensure it's on top.
       ctx.fillStyle = "#000000";
       ctx.fillText(prediction.class, x, y);
-    });
+    });*/
   };
 
   render() {
-    return (
+   return (
       <div>
-        <video
-          className="size"
-          autoPlay
-          playsInline
-          muted
-          ref={this.videoRef}
-          width="600"
-          height="500"
-        />
-        <canvas
-          className="size"
-          ref={this.canvasRef}
-          width="600"
-          height="500"
-        />
+        <div className={isMobile ? 'wrapper-mobile' : 'wrapper-desktop'}>
+          <video
+            className="size"
+            autoPlay
+            playsInline
+            muted
+            ref={this.videoRef}
+            width="400"
+            height="400"
+          />
+          <canvas
+            className="size"
+            ref={this.canvasRef}
+            width="400"
+            height="400"
+          />
+        </div>
+        <Message question={this.state.question} />
       </div>
     );
+  }
+}
+
+class Message extends React.Component {
+  render() {
+    return <div className={isMobile ? 'message-mobile' : 'message-desktop'}>
+      {questions[this.props.question].message}
+    </div>
   }
 }
 
